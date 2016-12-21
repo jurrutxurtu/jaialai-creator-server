5fb30df35904d955e88ff28cf12dee23b73077af
U-FIATC-ES\jon.urrutxurtu <jon.urrutxurtu@ENTPC03YR61.fiatc.es>
2016-12-21 18:53:54 +0100

    initial commit

---
+++ src/main/java/com/jaialai/JaialaiCreatorServerApplication.java
@@ -0,0 +1,14 @@
+package com.jaialai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaialaiCreatorServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JaialaiCreatorServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jaialai/JaialaiCreatorServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jaialai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaialaiCreatorServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
