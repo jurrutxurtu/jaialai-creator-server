bdf0bf0f37b05b69f8222aef717289fc1f813fb0
U-FIATC-ES\jon.urrutxurtu <jon.urrutxurtu@ENTPC03YR61.fiatc.es>
2016-12-22 16:37:49 +0100

    Adde yml for TravisCI

--- src/main/java/com/jaialai/Application.java
+++ src/main/java/com/jaialai/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JaialaiCreatorServerApplication
+public class Application
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(JaialaiCreatorServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
